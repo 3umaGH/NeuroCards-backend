c5a8b63de570e0879e8fe2de0381a5952b8196ba
3umaGH <3uma+github@protonmail.com>
2024-10-08 23:51:58 +0300

    feat: rate limiter

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { errorHandler } from './middleware/errorHandler'
 import { FlashCardRepository } from './repositories/flashCardRepository'
 import { FlashCardService } from './services/flashCardService'
 import { OpenAIService } from './services/openAiService'
+import { rateLimit } from 'express-rate-limit'
 
 const app = express()
 const port = 3000
@@ -19,13 +20,27 @@ app.use(
   })
 )
 
+export const generalLimiter = rateLimit({
+  windowMs: 60 * 1000, // 100 requests per minute
+  limit: 100,
+  standardHeaders: 'draft-7',
+  legacyHeaders: false,
+})
+
+export const quizSubmitLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 requests per 15 minutes.
+  limit: 10,
+  standardHeaders: 'draft-7',
+  legacyHeaders: false,
+})
+
 export const flashCardRepository = new FlashCardRepository(CONFIG)
 export const openAIService = new OpenAIService(flashCardRepository, CONFIG)
 export const flashCardService = new FlashCardService(flashCardRepository, CONFIG)
 export const flashCardController = new FlashCardController(flashCardService, openAIService)
 
-app.use('/v1/quiz', require('./routes/v1/quiz'))
-app.use('/v1/config', require('./routes/v1/config'))
+app.use('/v1/quiz', generalLimiter, require('./routes/v1/quiz'))
+app.use('/v1/config', generalLimiter, require('./routes/v1/config'))
 
 app.get('*', (req: Request, res: Response) => {
   res.status(404).json({
--- src/routes/v1/quiz.ts
+++ src/routes/v1/quiz.ts
@@ -1,10 +1,10 @@
-import { flashCardController } from '../..'
+import { flashCardController, quizSubmitLimiter } from '../..'
 import express from 'express'
 
 const router = express.Router()
 
 router.get('/:id', flashCardController.getQuizById)
 router.get('/', flashCardController.getRecentQuizzes)
-router.post('/', flashCardController.createQuiz)
+router.post('/', quizSubmitLimiter, flashCardController.createQuiz)
 
 module.exports = router
