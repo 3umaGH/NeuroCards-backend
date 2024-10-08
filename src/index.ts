04fee1ba44b847265565781743ea836b9c5fcd43
3umaGH <3uma+github@protonmail.com>
2024-10-08 15:22:17 +0300

    fix: error handler order

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
-app.use(errorHandler)
 
 app.use(
   cors({
@@ -35,3 +34,5 @@ app.get('*', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
+
+app.use(errorHandler)
